aa8dba70451ad6bca642fee96b3e0b2bcdb33281
Emily Wall <emilywall@gatech.edu>
2016-07-19 08:40:10 -0700

    changed variance of numerical attributes to compute f-test

--- src/ial.js
+++ src/ial.js
@@ -78,7 +78,6 @@
                     // if there are 3 or fewer values, define it as categorical
                     if (uniqueVals.size < 4) isCategorical = true;
 
-                    // note: mean and variance are based on NORMALIZED attribute values
                     if (isCategorical) {
                         this.attributeValueMap[attribute] = {
                             'min': passedData[0][attribute],
@@ -1865,13 +1864,13 @@
     }
 
 // bias is defined as the variance between the data that has been examined
-// indNumThreshold (optional) indicates the maximum percentage change in variance that is tolerated for numerical attributes (defaults to 0.5)
+// indNumThreshold (optional) indicates the p-value below which the f-test would reject the hypothesis that the data items interacted with and the actual distribution are the same for numerical attributes (defaults to 0.1)
 // indCatThreshold (optional) indicates the p-value below which the chi-squared test would reject the hypothesis that the data items interacted with and the actual distribution are the same for categorical attributes (defaults to 0.1)
 // percAttrThreshold (optional) indicates what number or percentage of attributes can be below indThreshold (defaults to 0.5)
 // interactionTypes (optional) limits scope of computation to particular interaction types or all if left unspecified
     ial.computeVarianceBias = function(indNumThreshold, indCatThreshold, percAttrThreshold, time, interactionTypes) {
         var attributeValueMap = ial.getAttributeValueMap();
-        if (typeof indNumThreshold === 'undefined' || isNaN(parseFloat(indNumThreshold)) || indNumThreshold < 0 || indNumThreshold > 1) indNumThreshold = 0.5;
+        if (typeof indNumThreshold === 'undefined' || isNaN(parseFloat(indNumThreshold)) || indNumThreshold < 0 || indNumThreshold > 1) indNumThreshold = 0.1;
         if (typeof indCatThreshold === 'undefined' || isNaN(parseFloat(indCatThreshold)) || indCatThreshold < 0 || indCatThreshold > 1) indCatThreshold = 0.1;
         if (typeof percAttrThreshold === 'undefined' || isNaN(parseFloat(percAttrThreshold)) || percAttrThreshold < 0 || percAttrThreshold > Object.keys(attributeValueMap).length) percAttrThreshold = 0.5;
         var interactionSubset = getInteractionQueueSubset(time, interactionTypes);
@@ -1891,15 +1890,61 @@
         var numAttributes = 0;
         var numViolations = 0;
         var varianceVector = {};
+
         for (var attr in attributeValueMap) {
             varianceVector[attr] = {};
             if (attributeValueMap[attr].dataType == 'numeric') {
                 numAttributes++;
                 var curVariance = Number(computeAttributeVariance(dataSubset, attr));
-                var curChange = Math.abs((curVariance - Number(attributeValueMap[attr]['variance'])) / Number(attributeValueMap[attr]['variance']));
-                if (curChange > indNumThreshold) numViolations++;
-                varianceVector[attr]["variance"] = curVariance;
-                varianceVector[attr]["percent_change"] = curChange;
+                var fullVariance = Number(attributeValueMap[attr]['variance']);
+
+                function beta2(testParam, df1, df2) {
+                    var a0 = 0;
+                    var b0 = 1;
+                    var a1 = 1;
+                    var b1 = 1;
+                    var m1 = 0;
+                    var a2 = 0;
+                    var c1;
+                    while (Math.abs((a1 - a2) / a1) > .00001) {
+                        a2 = a1;
+                        c1 = -(df1 + m1) * (df1 + df2 + m1) * testParam / (df1 + 2 * m1) / (df1 + 2 * m1 + 1);
+                        a0 = a1 + c1 * a0;
+                        b0 = b1 + c1 * b0;
+                        m1++;
+                        c1 = m1 * (df2 - m1) * testParam / (df1 + 2 * m1 - 1) / (df1 + 2 * m1);
+                        a1 = a0 + c1 * a1;
+                        b1 = b0 + c1 * b1;
+                        a0 /= b1;
+                        b0 /= b1;
+                        a1 /= b1;
+                        b1 = 1;
+                    }
+                    return a1 / df1;
+                }
+
+                function beta1(testParam, df1, df2) {
+                    var prob;
+                    var temp = df1 + df2;
+                    var bt = Math.exp(ial.utils.logGamma(temp) - ial.utils.logGamma(df2) - ial.utils.logGamma(df1) + df1 * Math.log(testParam) + df2 * Math.log(1 - testParam));
+                    if (testParam < (df1 + 1) / (temp + 2)) prob = bt * beta2(testParam, df1, df2);
+                    else prob = 1 - bt * beta2(1 - testParam, df2, df1);
+                    return prob;
+                }
+
+                var fValue = 1.0 / (curVariance / fullVariance);
+                dfFull = this.dataSet.length - 1; 
+                dfSub = dataSubset.length - 1; 
+                var testParam = fValue / (fValue + dfSub / dfFull);
+                var result = beta1(testParam, dfFull / 2, dfSub / 2);
+                result = Math.round(result * 100000) / 100000;
+
+                varianceVector[attr]["degrees_of_freedom_1_full"] = dfFull; 
+                varianceVector[attr]["degrees_of_freedom_2_sub"] = dfSub;
+                varianceVector[attr]["f_value"] = fValue;
+                varianceVector[attr]["p_value"] = result; 
+                if (result < indNumThreshold) numViolations++;
+
             } else if (attributeValueMap[attr].dataType == 'categorical') {
                 numAttributes++;
                 // variance for categorical attributes returns chi-squared test
@@ -1916,9 +1961,8 @@
 
                 var gamma = function(x, df) {
                     var gammaResult;
-                    if (x <= 0) {
-                        gammaResult = 0;
-                    } else if (x < df + 1) {
+                    if (x <= 0) gammaResult = 0;
+                    else if (x < df + 1) {
                         var t1 = 1 / df;
                         var prob = t1;
                         var count = 1;
@@ -1927,7 +1971,7 @@
                             prob = prob + t1;
                             count = count + 1;
                         }
-                        prob = prob * Math.exp(df * Math.log(x) - x - logGamma(df));
+                        prob = prob * Math.exp(df * Math.log(x) - x - ial.utils.logGamma(df));
                         return prob;
                     } else {
                         var a0 = 0;
@@ -1948,20 +1992,14 @@
                             a1 = a1 / b1;
                             b1 = 1;
                         }
-                        var prob = 1.0 - (Math.exp(df * Math.log(x) - x - logGamma(df)) * a1);
+                        var prob = 1.0 - (Math.exp(df * Math.log(x) - x - ial.utils.logGamma(df)) * a1);
                         return prob;
                     }
 
                     return gammaResult;
                 }
 
-                var logGamma = function(x) {
-                    var curVal = 1 + 76.18009173 / x - 86.50532033 / (x + 1) + 24.01409822 / (x + 2) - 1.231739516 / (x + 3) + .00120858003 / (x + 4) - .00000536382 / (x + 5);
-                    var logRes = (x - .5) * Math.log(x + 4.5) - (x + 4.5) + Math.log(curVal * 2.50662827465);
-                    return logRes;
-                }
-
-                result = gamma(chiSq / 2, degFree / 2);
+                var result = gamma(chiSq / 2, degFree / 2);
                 result = 1.0 - (Math.round(result * 100000) / 100000);
                 varianceVector[attr]["degrees_of_freedom"] = degFree; 
                 varianceVector[attr]["chi_squared"] = chiSq;
@@ -2107,6 +2145,12 @@
      * */
 
     ial.utils = {}
+    ial.utils.logGamma = function(x) {
+        var curVal = 1 + 76.18009173 / x - 86.50532033 / (x + 1) + 24.01409822 / (x + 2) - 1.231739516 / (x + 3) + .00120858003 / (x + 4) - .00000536382 / (x + 5);
+        var logRes = (x - .5) * Math.log(x + 4.5) - (x + 4.5) + Math.log(curVal * 2.50662827465);
+        return logRes;
+    }
+
     ial.utils.clone = function(obj) {
         // Handle the 3 simple types, and null or undefined
         if (null == obj || "object" != typeof obj) return obj;
